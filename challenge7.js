fd6a1313dd4b4ee13038c064fa4c65902c05fa3a
hilmihs <hilmisobandi@gmail.com>
2022-05-27 22:39:06 +0700

    challenge 1-7 done

--- challenge7.js
+++ challenge7.js
@@ -1,16 +1,24 @@
 function weirdMultiply(sentence) {
     let keString = sentence.toString();
-   
     let arraySen = keString.split("");
-   //console.log(arraySen) 
    var z = 1
+   var arr = []
     for (let i = 0; i < arraySen.length; i++) {
         var x = parseInt(arraySen[i]);
         z *= x;
-    } return z;
+       
+    } arr.push[z]
+    console.log[arr]
+    
+    if (z.toString().length > 1) {
+        return weirdMultiply(z)
+    } else if (z.toString().length === 1) {
+    return z
+    } 
+    
 } 
 
 
-console.log(weirdMultiply(39));
+console.log(weirdMultiply(39))
 console.log(weirdMultiply(999));
-console.log(weirdMultiply(9))
+console.log(weirdMultiply(3))
