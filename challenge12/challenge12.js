0ec2b40588ce5fb1f1f99501a6ece6f0c27aaf3e
hilmihs <hilmisobandi@gmail.com>
2022-06-08 09:08:39 +0700

    challenge 1-15 done

--- challenge12/challenge12.js
+++ challenge12/challenge12.js
@@ -6,7 +6,6 @@ if (process.argv.length == 3) {
         prompt: 'Tebakan: '
     });
     const fs = require('fs');
-
     const jawaban = JSON.parse(fs.readFileSync('data.json', 'utf-8'))
     console.log(`Selamat datang di permainan Tebak-tebakan, kamu akan diberikan pertanyaan dari file ini '${process.argv[2]}'.`)
     console.log('Untuk bermain jawablah dengan jawaban yang sesuai.')
@@ -19,27 +18,20 @@ if (process.argv.length == 3) {
     var i = 0
     rl.on('line', (isi) => {
         var answer = isi.trim();
-
         if (answer.toLowerCase() == jawaban[i].term) {
-
             console.log('Anda Beruntung!')
             jawaban.splice(i, 1);
             if (i == jawaban.length) {
                 i = 0;
             }
             console.log(jawaban)
-
             if (jawaban.length >= 1) {
                 console.log(`Pertanyaan:  ${jawaban[i].definition}`)
-
             }
             if (jawaban.length == 0) {
                 console.log('Hore Anda Menang!')
                 rl.close()
-
             }
-
-
         } else if (answer.toLowerCase() == 'skip') {
             i++
             console.log(i)
@@ -47,16 +39,13 @@ if (process.argv.length == 3) {
                 i = 0;
             }
             console.log(`Pertanyaan:  ${jawaban[i].definition}`);
-
         }
         else {
             falseCount++
             console.log(`Anda kurang beruntung! anda telah salah ${falseCount} kali, silahkan coba lagi.`)
-
         }
         rl.prompt();
     }
-
     )
 } else {
     console.log('Tolong sertakan nama file sebagai inputan soalnya')
